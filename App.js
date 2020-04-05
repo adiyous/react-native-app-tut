54d84e30e3e15229c87349dbe4cadad9d6df9790
adiyous <adilnewyork@gmail.com>
2020-04-04 21:51:42 -0300

    Adding stylesheet

--- App.js
+++ App.js
@@ -2,8 +2,25 @@ import React from 'react'
 import {
   View,
   Text,
-  StatusBar
+  StatusBar,
+  StyleSheet,
+  TouchableWithoutFeedback
 } from 'react-native'
+
+const styles = StyleSheet.create({
+  defaultText: {
+    fontSize: 22,
+    padding: 10,
+    margin: 5,
+    color: 'black',
+    borderWidth: StyleSheet.hairlineWidth
+  },
+  selectedText: {
+    color: 'blue',
+    backgroundColor: 'yellow',
+    fontWeight: 'bold'
+  }
+})
 
 // class App extends React.component {
 const App: () => React$Node = () => {
@@ -11,7 +28,14 @@ const App: () => React$Node = () => {
     return (
       <View>
         <StatusBar hidden={true} />
-        <Text>Hello React</Text>
+        <Text style={[
+                styles.defaultText,
+                styles.selectedText
+              ]}>
+                  Sierra
+        </Text>
+        <Text style={styles.defaultText}>Tanner</Text>
+        <Text style={styles.defaultText}>Travis</Text>
       </View>
     )
   //}
