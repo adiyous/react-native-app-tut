d8d0c9acb516d67e0dba88a921d6b274277f1037
adiyous <adilnewyork@gmail.com>
2020-04-04 21:07:56 -0300

    Starting with simple text and View

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// class App extends React.component {
+const App: () => React$Node = () => {
+//  render() {
+    return (
+      <View>
+        <Text>Hello React</Text>
+      </View>
+    );
+//  }
+}
+
+export default App;
