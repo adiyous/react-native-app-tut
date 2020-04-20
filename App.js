3fbc2ff57d4424f4ba4b1daebacb2aa3be6a0e5a
adiyous <adilnewyork@gmail.com>
2020-04-19 23:01:15 -0300

    Adding a navigator

--- App.js
+++ App.js
@@ -2,21 +2,42 @@ import React from 'react'
 
 import ColorList from './components/ColorList'
 import ColorInfo from './components/ColorInfo'
-import { Alert } from 'react-native'
+import { Alert, Navigator } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 
-const App = StackNavigator({
-  Home: { screen: ColorList },
-  Details: { screen: ColorInfo }
-})
-// export default class App extends React.Component {
-//   render(){
-//     return(
-//       <ColorList 
-//         onColorSelected={color => Alert.alert(color)}
-//       />
-//     )
-//   }
-// }
+// const App = StackNavigator({
+//   Home: { screen: ColorList },
+//   Details: { screen: ColorInfo }
+// })
+
+export default class App extends React.Component {
+  render(){
+    return(
+      // <ColorList 
+      //   onColorSelected={color => Alert.alert(color)}
+      // />
+      <Navigator 
+        initialRoute={{name: 'Color List'}}
+        renderScene={(route, navigator) => {
+          switch(route.name){
+            case 'Color Info':
+              return <ColorInfo 
+                        backgroundColor={route.color}
+                        onSelect={() => navigator.pop()} />
+            default:
+              return <ColorList 
+                        onColorSelected={
+                            (color) => navigator.push({
+                              name: 'Color Info',
+                              color
+                            })
+                        }
+                        />
+          }
+        }}
+        />
+    )
+  }
+}
 
 AppRegistry.registerComponent('SampleApp', () => App)
